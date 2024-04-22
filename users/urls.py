1d5f4ccc4dc78ceba5bc4f892bcb0a0d3b191a18
Miguel <msb2@cesar.school>
2024-04-22 13:30:02 -0300

    login parcialmente implementado

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns=[
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('plataforma', views.plataforma, name="plataforma"),
+    path('index', views.index, name='index')
 ]
