14aefe5985a416028637efbe8f5d19d6bdc91c82
Migueldsbatista <msb2@cesar.school>
2024-04-25 09:50:55 -0300

    commit

--- Rec-Tech/rt_app/views.py
+++ Rec-Tech/rt_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Lixeira, Aviso
 from django.contrib.auth.decorators import login_required
+from rolepermissions.decorators import has_role_decorator
+from rolepermissions.decorators import has_permission_decorator
+from rt_project.roles import Cliente, Admin, Coletor
 
 
 @login_required(login_url="/auth/login/")
@@ -9,13 +12,15 @@ def visualizar_lixeiras_e_avisos(request):
     avisos = Aviso.objects.all()
     return render(request, 'lixeiras.html', {'lixeiras': lixeiras, 'avisos': avisos})
 
-
+@has_role_decorator(Coletor)
 def coletor(request):
     return redirect("rt_app/coletor.html")
 
+@has_role_decorator(Cliente)
 def cliente(request):
     return render(request, "rt_app/cliente.html")
 
+@has_role_decorator(Admin)
 def admin(request):
     return render(request, "rt_app/admin.html")
 
