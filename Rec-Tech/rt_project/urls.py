457725decb596cb640b5e3d19cad20f183af6006
Migueldsbatista <msb2@cesar.school>
2024-04-27 17:24:48 -0300

    urls again

--- Rec-Tech/rt_project/urls.py
+++ Rec-Tech/rt_project/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rt_app/', include('rt_app.urls')),  # Change from '/' to 'rt_app/'
     path('auth/', include("users.urls")),
     path('', include('users.urls')),
-    path('/', include('rt_app.urls'))
 
 ]
