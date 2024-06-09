2b355a3f17d2117e1d6ff8b8908dcda1078ef5ba
Migueldsbatista <msb2@cesar.school>
2024-06-09 18:13:36 -0300

    historia do coletor  finalizada

--- Rec-Tech/coletor_app/models.py
+++ Rec-Tech/coletor_app/models.py
@@ -5,5 +5,10 @@ from django.contrib.auth.models import User
 class Coletor(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name="coletor", null=True)
     email = models.EmailField(null=True)
+    coletas_realizadas = models.IntegerField(default=0)
+    peso_coletado = models.FloatField(default=0)
 
+    
+    def __str__(self):
+        return f"{self.usuario} - {self.email}"
 
